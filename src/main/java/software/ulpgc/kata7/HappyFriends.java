1f56587c07b6c797ede2b403d3ad0075cbcfe0a1
Roberto-f55 <robertolzte90@gmail.com>
2025-12-27 12:27:16 +0000

    kata7 finished

--- src/main/java/software/ulpgc/kata7/HappyFriends.java
+++ src/main/java/software/ulpgc/kata7/HappyFriends.java
@@ -1,4 +1,27 @@
 package software.ulpgc.kata7;
 
-public class HappyFriends {
+import javax.swing.*;
+import java.awt.*;
+
+public class HappyFriends extends JFrame {
+    public static void main(String[] args) {
+        System.out.println(System.getenv("NUMBER_OF_PROCESSORS"));
+        new HappyFriends().setVisible(true);
+    }
+
+    public HappyFriends() throws HeadlessException {
+        this.setTitle("Kata 7");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(800,600);
+        this.setLocationRelativeTo(null);
+
+        ImageIcon imagen = new ImageIcon(getClass().getResource("/image.jpg"));
+
+        JLabel label = new JLabel(imagen);
+
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        label.setVerticalAlignment(SwingConstants.CENTER);
+
+        this.add(label);
+    }
 }
